32b52023ad6a5cfedd0bef26791a6c77a07128b6
Denali Balser <denali.balser167@gmail.com>
2019-05-19 00:14:24 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,7 +17,8 @@ require 'pry'
 def remove_strawberry(contacts)
   contacts.each do |person, data|
     data.each do |attribute, value|
-      binding.pry
+      if attribute == :favorite_ice_cream_flavors
+
     end
   end
 end
